cdce810334a003a026f4816b3055d94955335dfd
kashif <kashif.tufail17@gmail.com>
2025-07-18 15:03:42 +0500

    add questions form and model

---
+++ src/app/questions/new/page.js
@@ -0,0 +1,9 @@
+import NewQuestion from '@/components/NewQuestion';
+
+export default function NewQuestionPage() {
+  return (
+    <div className="min-h-screen p-6 bg-gray-50">
+      <NewQuestion />
+    </div>
+  );
+}
